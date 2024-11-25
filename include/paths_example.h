d0f98f7aaa892c80bffdf9dfc7f00b8cba21850a
rjsaito0519 <ryuta.saito.q3@dc.tohoku.ac.jp>
2024-11-25 11:55:25 +0900

    Mon, Nov 25, 2024 11:55:25 AM

---
+++ include/paths_example.h
@@ -0,0 +1,7 @@
+#ifndef PATHS_EXAMPLE_H
+#define PATHS_EXAMPLE_H
+
+const std::string DATA_PATH = "/path/to/data/env1/";
+const std::string OUTPUT_PATH = "/path/to/output/env1/";
+
+#endif // PATHS_EXAMPLE_H
